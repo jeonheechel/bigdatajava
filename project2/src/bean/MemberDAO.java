5678aad076efa1e3bffbaa12d4f4a0ac1e5d3445
jeonheechel <49705374+jeonheechel@users.noreply.github.com>
2019-06-05 15:21:00 +0900

    Add files via upload

---
+++ project2/src/bean/MemberDAO.java
@@ -0,0 +1,36 @@
+package bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class MemberDAO {
+
+	//dao의 단계를 조금 효율적으로 변경
+	public void insert(MemberDTO dto) throws Exception {
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("드라이버 설정 ok<br>");
+		String url = "jdbc:mysql://localhost:3306/member";
+		String user = "root";
+		String pwd = "1234";
+		
+		Connection con = null;
+		PreparedStatement ps = null;
+		con = DriverManager.getConnection(url, user, pwd);
+		
+		String sql = "insert into user values(?,?,?,?,?,?,?,?,?)";
+		ps = con.prepareStatement(sql);
+		
+		ps.setString(1,dto.getId());
+		ps.setString(2,dto.getPw());
+		ps.setString(3,dto.getTel());
+		ps.setString(4,dto.getAddr());
+		ps.setString(5,dto.getTall());
+		ps.setString(6,dto.getKg());
+		ps.setString(7,dto.getSex());
+		ps.setString(8,dto.getAge());
+		ps.setString(9,dto.getEmail());
+		ps.executeUpdate();
+}
+}
---
+++ project2/src/bean/MemberDTO.java
@@ -0,0 +1,76 @@
+package bean;
+
+public class MemberDTO {
+	private String id;
+	private String pw;
+	private String tel;
+	private String addr;
+	private String tall;
+	private String kg;
+	private String sex;
+	private String age;
+	private String email;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getTall() {
+		return tall;
+	}
+	public void setTall(String tall) {
+		this.tall = tall;
+	}
+	public String getKg() {
+		return kg;
+	}
+	public void setKg(String kg) {
+		this.kg = kg;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+	
+	
+
+}
